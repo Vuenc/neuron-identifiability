373c3e7cf64a2690a7cdebc6ebbc47607cebc0b3
Vincent Bürgin <vincent.buergin@tum.de>
2026-03-24 00:16:30 +0100

    Updated measure_subspace_coherence.py

--- measure_subspace_coherence.py
+++ measure_subspace_coherence.py
@@ -1,12 +1,17 @@
 import concurrent.futures
+from typing import Dict, List
 import hydra
 import torch
 import pathlib
+from src.utils.record_activations import MODEL_OUTPUT_RECORDING_POINT, ActivationRecordingPoint, RecordInput
 import train
 import src.utils.rebasin.subspace_coherence
-import numpy as np
+from src.utils.rebasin.subspace_coherence import SubspaceCoherenceResult
 from contextlib import contextmanager
 from checkpoint_directories import checkpoint_directories_by_architecture
+import tqdm
+import json
+import argparse
 
 @contextmanager
 def suppress_prints(suppress=True):
@@ -25,49 +30,78 @@ def suppress_prints(suppress=True):
 #     "mlp_symmetry0": "outputs/2026-01-15/17-07-22_mlp_mnist_sym-0__db8ehws3__y5oujjmq",
 # }
 
-checkpoint_directories = checkpoint_directories_by_architecture["mlp"]
-
 def compute_subspace_coherence_results(checkpoint_path, device="cuda:0", data_info=None):
     with hydra.initialize(version_base=None, config_path=str(pathlib.Path(checkpoint_path).parent)):
         cfg = hydra.compose(config_name="config")
     cfg.dataset.batch_size = 500
+    print(cfg.dataset)
     if data_info is None:
         data_info = train.setup_data_loaders(cfg)
 
     model = train.create_model(cfg, mask_seed=-1).to(device)
     model.load_state_dict(torch.load(checkpoint_path, map_location=device)["model_state_dict"])
 
-    permutation_spec = src.utils.rebasin.mlp_permutation_spec(3, norm=True, bias=True)
-    coherence_results = src.utils.rebasin.subspace_coherence.compute_representation_subspace_coherences(
-        permutation_spec, model, data_info["train_loader"], target_explained_variance_ratio=0.9, device=device
+    activation_recording_points = [("lins.0", RecordInput), ("lins.1", RecordInput), ("lins.2", RecordInput), ("lins.3", RecordInput), MODEL_OUTPUT_RECORDING_POINT]
+    coherence_results: Dict[ActivationRecordingPoint, SubspaceCoherenceResult] = (
+        src.utils.rebasin.subspace_coherence.compute_representation_subspace_coherences(
+            activation_recording_points=activation_recording_points,
+            model=model,
+            data_loader=data_info["train_loader"],
+            target_explained_variance_ratio=0.9,
+            device=device
+        )
     )
     results = []
     with torch.no_grad():
-        for matching_mode, results_per_layer in coherence_results.items():
-            for layer_name, coherence_result in results_per_layer.items():
+        for recording_point, coherence_result in coherence_results.items():
+                layer_name, hook_mode = recording_point
                 results.append({
-                    "matching_mode": matching_mode,
-                    "layer": layer_name,
+                    "layer": layer_name if layer_name != "" else ("model-input" if hook_mode == RecordInput else "model-output"),
+                    "hook_mode": hook_mode.name,
                     "subspace_coherence": coherence_result.subspace_coherence,
                     "subspace_dimension": coherence_result.subspace_dimension,
                     "full_dimension": coherence_result.full_dimension,
                     "explained_variance_ratio": coherence_result.explained_variance_ratio,
+                    "anisotropy_operator_norms": coherence_result.anisotropy_operator_norms,
+                    "mean_anisotropy_operator_norm": coherence_result.mean_anisotropy_operator_norm,
+                    "anisotropy_bounds": coherence_result.anisotropy_bounds,
                 })
     return results
 
-MODEL_RANGE = list(range(1, 17, 1))
+# MODEL_RANGE = list(range(1, 17, 1))
+MODEL_RANGE = [1]
+# EPOCH_RANGE = list(range(0, 101, 5))
 EPOCH_RANGE = list(range(0, 101, 5))
+# EPOCH_RANGE = [100]
 # EPOCH_RANGE = list(range(0, 11, 1))
 MAX_PARALLEL_PROCESSES = 22
 
 all_results = []
-import tqdm
-import json
+
+parser = argparse.ArgumentParser(prog="measure_subspace_coherence.py")
+parser.add_argument("--output-file", type=str, required=True)
+parser.add_argument("--architecture", type=str)
+parser.add_argument("--checkpoint-directory", type=str)
+parser.add_argument("--run-key")
+parser.add_argument("--no-suppress-prints", action="store_true")
+parser.add_argument("--parallel-processes", type=int, default=MAX_PARALLEL_PROCESSES)
+args = parser.parse_args()
+
+if (args.checkpoint_directory is None) != (args.run_key is None):
+    raise ValueError("Arguments --checkpoint-directory and --run-key must both be specified if one of them is specified.")
+if (args.checkpoint_directory is None) == (args.architecture is None):
+    raise ValueError("Exactly one of the arguments --architecture and --checkpoint-directory must be specified!")
+
+if args.architecture:
+    checkpoint_directories = checkpoint_directories_by_architecture[args.architecture]
+else:
+    checkpoint_directories = {args.run_key: args.checkpoint_directory}
 
 # Assuming all are using the same dataset
 with hydra.initialize(version_base=None, config_path=str(pathlib.Path(list(checkpoint_directories.values())[0]))):
     _cfg = hydra.compose(config_name="config")
 _cfg.dataset.batch_size = 2**15
+EPOCH_RANGE = [epoch for epoch in EPOCH_RANGE if epoch <= _cfg.training.num_epochs] or [_cfg.training.num_epochs]
 data_info = train.setup_data_loaders(_cfg)
 
 checkpoint_path = lambda model_index, epoch: f"{checkpoint_directories[run_key]}/checkpoint_epoch_{epoch}_model_{model_index}.pt"
@@ -76,15 +110,19 @@ for run_key in (tqdm_run := tqdm.tqdm(checkpoint_directories.keys())):
     tqdm_run.set_description(f"Run: {run_key}")
     for model_index in (tqdm_model := tqdm.tqdm(MODEL_RANGE, desc=run_key, leave=False)):
         tqdm_model.set_description(f"Model {model_index}")
-        with suppress_prints(suppress=True):
-            with concurrent.futures.ProcessPoolExecutor(max_workers=MAX_PARALLEL_PROCESSES) as executor:
-                # import types
-                # executor = types.SimpleNamespace(submit=lambda f, **kwargs: types.SimpleNamespace(result = lambda: f(**kwargs)))
-                model_results = [future.result() for future in [
-                    executor.submit(compute_subspace_coherence_results,
-                        **dict(checkpoint_path=checkpoint_path(model_index, epoch), data_info=data_info)
-                    ) for epoch in EPOCH_RANGE]
-                ]
+        with suppress_prints(suppress=not args.no_suppress_prints):
+            if args.parallel_processes > 1:
+                executor = concurrent.futures.ProcessPoolExecutor(max_workers=args.parallel_processes)
+            else:
+                import types
+                executor = types.SimpleNamespace(submit=lambda f, **kwargs: types.SimpleNamespace(result = lambda: f(**kwargs)))
+            model_results = [future.result() for future in [
+                executor.submit(compute_subspace_coherence_results,
+                    **dict(checkpoint_path=checkpoint_path(model_index, epoch), data_info=data_info)
+                ) for epoch in EPOCH_RANGE]
+            ]
+            if args.parallel_processes > 1:
+                executor.shutdown()
         for coherence_results, epoch in zip(model_results, EPOCH_RANGE):
             all_results.append({
                 "run_key": run_key,
@@ -92,5 +130,5 @@ for run_key in (tqdm_run := tqdm.tqdm(checkpoint_directories.keys())):
                 "epoch": epoch,
                 "coherence_results": coherence_results
             })
-        with open("outputs/subspace-coherence-results-mlp.json", "w") as f:
+        with open(args.output_file, "w") as f:
             json.dump(all_results, f)
